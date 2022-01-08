d447148f66cf5336fce5c9576c28b2290cd3e6c2
Li-xue <lixuec@gmail.com>
2022-01-08 14:25:15 -0800

    Second graph created

--- app.py
+++ app.py
@@ -33,14 +33,29 @@ for i in time:
     thistime = thistime - datetime.timedelta(hours=8)
     finaltime.append(thistime.strftime('%H:%M'))
 df['time'] = finaltime
-# ------------------------------------------------------------------------------
+# ==============================================================================
 # Sort and group the times
+# ==============================================================================
+# ------------------------------------------------------------------------------
+# Means/stdv
 # ------------------------------------------------------------------------------
 df.sort_values('time',inplace=True)
 meandf = df.groupby('time').mean()
 stdvdf = df.groupby('time').std()
 highdf = meandf + 2*stdvdf
 lowdf = meandf - 2*stdvdf
+# ------------------------------------------------------------------------------
+# Q1/Q3 with mean graph
+# ------------------------------------------------------------------------------
+qrtrdf = df.groupby('time').quantile([0.25,0.75]).unstack()
+qrtrdf.columns = ['go_q1','go_q3','return_q1','return_q3']
+minmaxdf = df[['time','go','return']].groupby('time').agg(['min','max'])
+minmaxdf.columns = ['go_min','go_max','return_min','return_max']
+finaldf2 = pd.concat([meandf,qrtrdf,minmaxdf],axis=1)
+returndf2 = finaldf2[['return','return_q1','return_q3','return_min','return_max']].copy()
+goingdf2 = finaldf2[['go','go_q1','go_q3','go_min','go_max']].copy()
+returndf2['time'] = returndf2.index.to_list()
+goingdf2['time'] = goingdf2.index.to_list()
 # ------------------------------------------------------------------------------
 # Set up the final dataframe
 # ------------------------------------------------------------------------------
@@ -69,6 +84,29 @@ goingfig.add_trace(go.Scatter(x=goingdf['time'],y=goingdf['go_high'],name='high'
 goingfig.add_trace(go.Scatter(x=goingdf['time'],y=goingdf['go'],name='mean',line_shape='spline'))
 goingfig.add_trace(go.Scatter(x=goingdf['time'],y=goingdf['go_low'],name='low',line_shape='spline'))
 goingfig.update_layout(xaxis_title='Time of Day',yaxis_title='Time (min)')
+# ==============================================================================
+# Create the plots for min/max/mean/q1/q3
+# ==============================================================================
+# ------------------------------------------------------------------------------
+# Return graphs
+# ------------------------------------------------------------------------------
+returnfig2 = go.Figure()
+returnfig2.add_trace(go.Scatter(x=returndf2['time'],y=returndf2['return_q3'],name='q3',line_shape='spline'))
+returnfig2.add_trace(go.Scatter(x=returndf2['time'],y=returndf2['return'],name='mean',line_shape='spline'))
+returnfig2.add_trace(go.Scatter(x=returndf2['time'],y=returndf2['return_q1'],name='q1',line_shape='spline'))
+# returnfig2.add_trace(go.Scatter(x=returndf2['time'],y=returndf2['return_min'],name='min'))
+# returnfig2.add_trace(go.Scatter(x=returndf2['time'],y=returndf2['return_max'],name='max'))
+returnfig2.update_layout(xaxis_title='Time of Day',yaxis_title='Time (min)')
+# ------------------------------------------------------------------------------
+# Going graphs
+# ------------------------------------------------------------------------------
+goingfig2 = go.Figure()
+goingfig2.add_trace(go.Scatter(x=goingdf2['time'],y=goingdf2['go_q3'],name='q3',line_shape='spline'))
+goingfig2.add_trace(go.Scatter(x=goingdf2['time'],y=goingdf2['go'],name='mean',line_shape='spline'))
+goingfig2.add_trace(go.Scatter(x=goingdf2['time'],y=goingdf2['go_q1'],name='q1',line_shape='spline'))
+# goingfig2.add_trace(go.Scatter(x=goingdf2['time'],y=goingdf2['go_min'],name='min'))
+# goingfig2.add_trace(go.Scatter(x=goingdf2['time'],y=goingdf2['go_max'],name='max'))
+goingfig2.update_layout(xaxis_title='Time of Day',yaxis_title='Time (min)')
 
 # ==============================================================================
 # Building the page
@@ -78,9 +116,15 @@ goingfig.update_layout(xaxis_title='Time of Day',yaxis_title='Time (min)')
 # ------------------------------------------------------------------------------
 layout = []
 layout.append(html.H1(children='Return'))
+layout.append(html.H2(children='Mean and variance graph'))
 layout.append(dcc.Graph(figure=returnfig))
+layout.append(html.H2(children='Mean and Q1/Q3 graph'))
+layout.append(dcc.Graph(figure=returnfig2))
 layout.append(html.H1(children='Going'))
+layout.append(html.H2(children='Mean and variance graph'))
 layout.append(dcc.Graph(figure=goingfig))
+layout.append(html.H2(children='Mean and Q1/Q3 graph'))
+layout.append(dcc.Graph(figure=goingfig2))
 # ------------------------------------------------------------------------------
 # Going graphs
 # ------------------------------------------------------------------------------
